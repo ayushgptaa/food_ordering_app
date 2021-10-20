daeddeafda323807d00f856fa3b257b291a6f169
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-10-20 23:04:37 +0530

    🍦 Changed sidebar icons

--- src/layouts/dashboard/SidebarConfig.js
+++ src/layouts/dashboard/SidebarConfig.js
@@ -1,52 +1,42 @@
 import { Icon } from '@iconify/react';
-import pieChart2Fill from '@iconify/icons-eva/pie-chart-2-fill';
 import peopleFill from '@iconify/icons-eva/people-fill';
 import shoppingBagFill from '@iconify/icons-eva/shopping-bag-fill';
-import fileTextFill from '@iconify/icons-eva/file-text-fill';
-import lockFill from '@iconify/icons-eva/lock-fill';
-import personAddFill from '@iconify/icons-eva/person-add-fill';
-import alertTriangleFill from '@iconify/icons-eva/alert-triangle-fill';
 
 // ----------------------------------------------------------------------
 
-const getIcon = (name) => <Icon icon={name} width={22} height={22} />;
+const getIcon = (name) => <Icon icon={name} width={30} height={30} />;
 
 const sidebarConfig = [
   {
-    title: 'dashboard',
+    title: 'Home',
     path: '/dashboard/app',
-    icon: getIcon(pieChart2Fill)
+    icon: getIcon('fe:home')
   },
   {
-    title: 'user',
+    title: 'Promote',
     path: '/dashboard/user',
     icon: getIcon(peopleFill)
   },
   {
-    title: 'product',
+    title: 'My orders',
     path: '/dashboard/products',
     icon: getIcon(shoppingBagFill)
   },
   {
-    title: 'blog',
+    title: 'Add menu',
     path: '/dashboard/blog',
-    icon: getIcon(fileTextFill)
+    icon: getIcon('feather:grid')
   },
   {
-    title: 'login',
+    title: 'Settings',
     path: '/login',
-    icon: getIcon(lockFill)
-  },
-  {
-    title: 'register',
-    path: '/register',
-    icon: getIcon(personAddFill)
-  },
-  {
-    title: 'Not found',
-    path: '/404',
-    icon: getIcon(alertTriangleFill)
+    icon: getIcon('feather:settings')
   }
+  // {
+  //   title: 'Not found',
+  //   path: '/404',
+  //   icon: getIcon(alertTriangleFill)
+  // }
 ];
 
 export default sidebarConfig;
