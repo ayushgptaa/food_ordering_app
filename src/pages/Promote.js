e58e3224e1c6b930afa0d6730eae9543eb1c6122
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-10-23 19:59:21 +0530

    🚀 Changed promote page

--- src/pages/Promote.js
+++ src/pages/Promote.js
@@ -1,51 +1,54 @@
 // material
-import { Container, Grid, Typography, Card } from '@mui/material';
+import { Container, Grid, Typography, Card, Box, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
-
+import { Icon } from '@iconify/react';
 // ----------------------------------------------------------------------
 
 const RootStyle = styled(Card)(({ theme }) => ({
   boxShadow: 'none',
   textAlign: 'center',
-  padding: theme.spacing(10, 10),
+  padding: theme.spacing(7, 0),
+  margin: theme.spacing(2, 2),
   color: theme.palette.primary.darker,
-  backgroundColor: theme.palette.primary.lighter
+  backgroundColor: theme.palette.primary.lighter,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '10px'
 }));
+
+const DownloadIcon = () => <Icon icon="bx:bxs-download" color="white" />;
 
 export default function Promote() {
   return (
-    <Container>
-      <Grid
-        container
-        spacing={3}
-        direction="column"
-        sx={{
-          borderBottom: 'none',
-          borderStyle: 'solid',
-          borderColor: 'primary.maingreen',
-          borderWidth: 1,
-          borderRadius: 1
-        }}
-        justifyContent="space-evenly"
-        alignItems="center"
-      >
+    <Card sx={{ py: 6, maxWidth: 'md', mx: 'auto', mt: { md: 5 }, mb: 0 }}>
+      <Grid container spacing={3} direction="row" justifyContent="space-evenly" alignItems="center">
         <Grid item xs={12} sm={6} md={5}>
           <RootStyle>
-            <Typography variant="subtitle2" sx={{ opacity: 0.72 }}>
-              TODAY AMOUNT
+            <Box component="img" src="/static/QRcode.svg" sx={{ width: 90 }} />
+            <Typography variant="h5" sx={{ opacity: 0.72 }}>
+              QR code
             </Typography>
-            <Typography variant="h3">hello</Typography>
+
+            <Button variant="contained" sx={{ py: 1, px: 4 }} endIcon={<DownloadIcon />}>
+              Download
+            </Button>
           </RootStyle>
         </Grid>
         <Grid item xs={12} sm={6} md={5}>
           <RootStyle>
-            <Typography variant="subtitle2" sx={{ opacity: 0.72 }}>
-              TODAY AMOUNT
+            {/* <Box component="img" src="/static/QRcode.svg" sx={{ width: 80 }} /> */}
+            <Icon icon="carbon:template" width="90" />
+            <Typography variant="h5" sx={{ opacity: 0.72 }}>
+              Template
             </Typography>
-            <Typography variant="h3">hello</Typography>
+            <Button variant="contained" sx={{ py: 1, px: 4 }} endIcon={<DownloadIcon />}>
+              Download
+            </Button>
           </RootStyle>
         </Grid>
       </Grid>
-    </Container>
+    </Card>
   );
 }
