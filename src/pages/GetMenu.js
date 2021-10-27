a8e1c3a7db3e3496c579b91a90d5622b499867d6
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-10-27 13:42:36 +0530

    🐱‍🏍 added get menu in pages

---
+++ src/pages/GetMenu.js
@@ -0,0 +1,16 @@
+// material
+import { Grid, Button, Container, Stack, Typography } from '@mui/material';
+// components
+import Page from '../components/Page';
+
+// ----------------------------------------------------------------------
+
+// ----------------------------------------------------------------------
+
+export default function AddMenu() {
+  return (
+    <Page title="Dashboard: Add Menu ">
+      <Container>Hello</Container>
+    </Page>
+  );
+}
