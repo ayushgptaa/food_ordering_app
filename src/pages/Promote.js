4cb3ce6516f730683fce549131da6d01f0ef4cd8
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-10-28 11:15:56 +0530

    🦐 minor fixing in promote page

--- src/pages/Promote.js
+++ src/pages/Promote.js
@@ -1,7 +1,8 @@
 // material
-import { Container, Grid, Typography, Card, Box, Button } from '@mui/material';
+import { Grid, Typography, Card, Box, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { Icon } from '@iconify/react';
+import Page from '../components/Page';
 // ----------------------------------------------------------------------
 
 const RootStyle = styled(Card)(({ theme }) => ({
@@ -22,33 +23,40 @@ const DownloadIcon = () => <Icon icon="bx:bxs-download" color="white" />;
 
 export default function Promote() {
   return (
-    <Card sx={{ py: 6, maxWidth: 'md', mx: 'auto', mt: { md: 5 }, mb: 0 }}>
-      <Grid container spacing={3} direction="row" justifyContent="space-evenly" alignItems="center">
-        <Grid item xs={12} sm={6} md={5}>
-          <RootStyle>
-            <Box component="img" src="/static/QRcode.svg" sx={{ width: 90 }} />
-            <Typography variant="h5" sx={{ opacity: 0.72 }}>
-              QR code
-            </Typography>
+    <Page title=" Dashboard : Promote">
+      <Card sx={{ py: 6, maxWidth: 'md', mx: 'auto', mt: { md: 5 }, mb: 0 }}>
+        <Grid
+          container
+          spacing={0}
+          direction="row"
+          justifyContent="space-evenly"
+          alignItems="center"
+        >
+          <Grid item xs={12} sm={6} md={5}>
+            <RootStyle>
+              <Box component="img" src="/static/QRcode.svg" sx={{ width: 90 }} />
+              <Typography variant="h5" sx={{ opacity: 0.72 }}>
+                QR code
+              </Typography>
 
-            <Button variant="contained" sx={{ py: 1, px: 4 }} endIcon={<DownloadIcon />}>
-              Download
-            </Button>
-          </RootStyle>
+              <Button variant="contained" sx={{ py: 1, px: 4 }} endIcon={<DownloadIcon />}>
+                Download
+              </Button>
+            </RootStyle>
+          </Grid>
+          <Grid item xs={12} sm={6} md={5}>
+            <RootStyle>
+              <Icon icon="carbon:template" width="90" />
+              <Typography variant="h5" sx={{ opacity: 0.72 }}>
+                Template
+              </Typography>
+              <Button variant="contained" sx={{ py: 1, px: 4 }} endIcon={<DownloadIcon />}>
+                Download
+              </Button>
+            </RootStyle>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6} md={5}>
-          <RootStyle>
-            {/* <Box component="img" src="/static/QRcode.svg" sx={{ width: 80 }} /> */}
-            <Icon icon="carbon:template" width="90" />
-            <Typography variant="h5" sx={{ opacity: 0.72 }}>
-              Template
-            </Typography>
-            <Button variant="contained" sx={{ py: 1, px: 4 }} endIcon={<DownloadIcon />}>
-              Download
-            </Button>
-          </RootStyle>
-        </Grid>
-      </Grid>
-    </Card>
+      </Card>
+    </Page>
   );
 }
