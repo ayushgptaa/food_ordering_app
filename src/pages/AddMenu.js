76a0ea28a300a40b2259300b655249da4e6f2938
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-10-29 19:30:43 +0530

    🧹 removed extra code from AddMenu

--- src/pages/AddMenu.js
+++ src/pages/AddMenu.js
@@ -1,26 +1,32 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import {
   Container,
   Tab,
   Tabs,
-  TextField,
-  Box,
-  Button,
-  Grid,
-  InboxIcon,
-  ListItemIcon,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  List,
-  Divider
+  // TextField,
+  Box
+  // Button,
+  // Grid,
+  // InboxIcon,
+  // ListItemIcon,
+  // ListItem,
+  // ListItemButton,
+  // ListItemText,
+  // List,
+  // Divider
 } from '@mui/material';
 // import { typography } from '@mui/system';
 import Page from '../components/Page';
+import AddItem from '../components/_dashboard/Addmenu/additem';
+import AddCategory from '../components/_dashboard/Addmenu/addcartegory';
 
 // // ----------------------------------------------------------------------
 
+const commonTabstyle = {
+  fontSize: 'subtitle1.fontSize'
+};
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -32,7 +38,7 @@ function TabPanel(props) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && <Box sx={{ p: 5 }}>{children}</Box>}
+      {value === index && <Box sx={{ p: { md: 5, xs: 2 } }}>{children}</Box>}
     </div>
   );
 }
@@ -52,7 +58,6 @@ function a11yProps(index) {
 
 export default function BasicTabs() {
   const [value, setValue] = useState(0);
-
   const handleChange = (event, newValue) => {
     console.log('handlechange', newValue);
     setValue(newValue);
@@ -63,47 +68,23 @@ export default function BasicTabs() {
       <Container>
         <Box sx={{ width: '100%' }}>
           <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <Tabs value={value} onChange={handleChange} aria-label="Menu tabs" centered="true">
-              <Tab label="Add Category" {...a11yProps(0)} />
-              <Tab label="Add Items" {...a11yProps(1)} />
-              <Tab label="Add Options" {...a11yProps(2)} />
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="Menu tabs"
+              sx={{ '> Tab': { fontSize: 'h5.fontSize' } }}
+              centered
+            >
+              <Tab label="Add Category" sx={{ ...commonTabstyle }} {...a11yProps(0)} />
+              <Tab label="Add Items" sx={{ ...commonTabstyle }} {...a11yProps(1)} />
+              <Tab label="Add Options" sx={{ ...commonTabstyle }} {...a11yProps(2)} />
             </Tabs>
           </Box>
           <TabPanel value={value} index={0}>
-            <Grid
-              component="form"
-              container
-              sx={{
-                '& > :not(style)': { m: 1, width: '25ch' }
-              }}
-              noValidate
-              autoComplete="off"
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-            >
-              <TextField id="outlined-basic" label="Enter Category" variant="outlined" />
-              <Button variant="contained" sx={{ py: 2, fontSize: 'subtitle1.fontSize' }}>
-                ADD
-              </Button>
-            </Grid>
-            <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-              <List>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary="Trash" />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton component="a" href="#simple-list">
-                    <ListItemText primary="Spam" />
-                  </ListItemButton>
-                </ListItem>
-              </List>
-            </Box>
+            <AddCategory />
           </TabPanel>
           <TabPanel value={value} index={1}>
-            Item Two
+            <AddItem />
           </TabPanel>
           <TabPanel value={value} index={2}>
             Item Three
