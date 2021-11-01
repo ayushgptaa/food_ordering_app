87319d330a81cbd9b7ad2d148848a5cc7259eb76
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-11-01 22:51:01 +0530

    🍾 Changed dashboard cards width

--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -19,17 +19,17 @@ export default function DashboardApp() {
             Hi, Welcome back
           </Typography>
         </Box>
-        <Grid container spacing={3} sx={{ mt: { xs: 2, md: 4 } }}>
-          <Grid item xs={12} sm={6} md={3}>
+        <Grid container spacing={3} sx={{ mt: { xs: 2, md: 0 } }}>
+          <Grid item xs={12} sm={6}>
             <TodayAmount />
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6}>
             <TodayOrders />
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6}>
             <TotalAmount />
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6}>
             <TotalOrders />
           </Grid>
         </Grid>
