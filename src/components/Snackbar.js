df9d291b480e933c3f05567d61cc4e75a913b6bc
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-11-03 10:36:55 +0530

    removed extra code from Snackbar

--- src/components/Snackbar.js
+++ src/components/Snackbar.js
@@ -1,20 +1,13 @@
-import { useState, forwardRef } from 'react';
+import { forwardRef } from 'react';
 import MuiSnackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 
+/* eslint-disable react/prop-types */
 const Alert = forwardRef((props, ref) => {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 export default function SnackBar({ open, varient, inputval, handleClose }) {
-  //   const handleClose = (event, reason) => {
-  //     if (reason === 'clickaway') {
-  //       return;
-  //     }
-
-  //     setOpen(false);
-  //   };
-
   return (
     <MuiSnackbar open={open} autoHideDuration={4000} onClose={handleClose}>
       <Alert
