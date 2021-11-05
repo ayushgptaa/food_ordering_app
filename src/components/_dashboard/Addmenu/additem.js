a3de7b05511a97afbb9480873c26aba85bf59995
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-11-05 23:48:22 +0530

    added category in additem.js

--- src/components/_dashboard/Addmenu/additem.js
+++ src/components/_dashboard/Addmenu/additem.js
@@ -1,7 +1,43 @@
-import { TextField, Grid, Button, Box, InputAdornment, Autocomplete } from '@mui/material';
+/* eslint-disable camelcase */
+import { useState } from 'react';
+import {
+  Select,
+  Grid,
+  Button,
+  Box,
+  InputAdornment,
+  MenuItem,
+  InputLabel,
+  FormControl
+} from '@mui/material';
+import PropTypes from 'prop-types';
 import CustomTextFeild from '../../TextField';
 
-export default function AddItem() {
+AddItem.propTypes = {
+  categories: PropTypes.arrayOf(
+    PropTypes.shape({
+      category: PropTypes.string,
+      category_id: PropTypes.string
+    })
+  )
+};
+// eslint-disable-next-line react/prop-types
+export default function AddItem({ categories }) {
+  const defaultStates = {
+    item_name: '',
+    item_description: '',
+    item_amount: '',
+    category_id: ''
+  };
+  const [input, setInput] = useState(defaultStates);
+
+  const inputhandler = (e) => {
+    const { value } = e.target;
+    setInput({
+      ...input,
+      [e.target.name]: value
+    });
+  };
   return (
     <Grid container direction="column" alignItems="center">
       <Box
@@ -9,20 +45,39 @@ export default function AddItem() {
           maxWidth: 400
         }}
       >
-        <Autocomplete
-          disablePortal
-          id="combo-box-demo"
-          options={top100Films}
-          autoHighlight
-          renderInput={(params) => <TextField {...params} label="Movie" />}
-        />
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Category</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={input.category_id}
+            name="category_id"
+            label="Category"
+            onChange={inputhandler}
+          >
+            {categories.map(({ category, category_id }) => {
+              return (
+                <MenuItem value={category_id} key={category_id}>
+                  {category}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
 
-        <CustomTextFeild label="Item Title" placeholder="Enter Item" />
+        <CustomTextFeild
+          label="Item Title"
+          placeholder="Enter Item"
+          onChange={inputhandler}
+          name="item_name"
+        />
         <CustomTextFeild
           label="Item Description"
           placeholder="Item Description"
           multiline
           rows={4}
+          onChange={inputhandler}
+          name="item_description"
         />
         <CustomTextFeild
           margin="dense"
@@ -31,10 +86,13 @@ export default function AddItem() {
           InputProps={{
             endAdornment: <InputAdornment position="end">$</InputAdornment>
           }}
+          onChange={inputhandler}
+          name="item_amount"
         />
         <Button
           variant="contained"
           sx={{ py: 1.5, mt: 2, fontSize: 'subtitle1.fontSize', width: '100%' }}
+          onClick={() => console.log(input)}
         >
           ADD
         </Button>
@@ -42,109 +100,3 @@ export default function AddItem() {
     </Grid>
   );
 }
-const top100Films = [
-  { label: 'The Shawshank Redemption', year: 1994 },
-  { label: 'The Godfather', year: 1972 },
-  { label: 'The Godfather: Part II', year: 1974 },
-  { label: 'The Dark Knight', year: 2008 },
-  { label: '12 Angry Men', year: 1957 },
-  { label: "Schindler's List", year: 1993 },
-  { label: 'Pulp Fiction', year: 1994 },
-  {
-    label: 'The Lord of the Rings: The Return of the King',
-    year: 2003
-  },
-  { label: 'The Good, the Bad and the Ugly', year: 1966 },
-  { label: 'Fight Club', year: 1999 },
-  {
-    label: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001
-  },
-  {
-    label: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980
-  },
-  { label: 'Forrest Gump', year: 1994 },
-  { label: 'Inception', year: 2010 },
-  {
-    label: 'The Lord of the Rings: The Two Towers',
-    year: 2002
-  },
-  { label: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { label: 'Goodfellas', year: 1990 },
-  { label: 'The Matrix', year: 1999 },
-  { label: 'Seven Samurai', year: 1954 },
-  {
-    label: 'Star Wars: Episode IV - A New Hope',
-    year: 1977
-  },
-  { label: 'City of God', year: 2002 },
-  { label: 'Se7en', year: 1995 },
-  { label: 'The Silence of the Lambs', year: 1991 },
-  { label: "It's a Wonderful Life", year: 1946 },
-  { label: 'Life Is Beautiful', year: 1997 },
-  { label: 'The Usual Suspects', year: 1995 },
-  { label: 'Léon: The Professional', year: 1994 },
-  { label: 'Spirited Away', year: 2001 },
-  { label: 'Saving Private Ryan', year: 1998 },
-  { label: 'Once Upon a Time in the West', year: 1968 },
-  { label: 'American History X', year: 1998 },
-  { label: 'Interstellar', year: 2014 },
-  { label: 'Casablanca', year: 1942 },
-  { label: 'City Lights', year: 1931 },
-  { label: 'Psycho', year: 1960 },
-  { label: 'The Green Mile', year: 1999 },
-  { label: 'The Intouchables', year: 2011 },
-  { label: 'Modern Times', year: 1936 },
-  { label: 'Raiders of the Lost Ark', year: 1981 },
-  { label: 'Rear Window', year: 1954 },
-  { label: 'The Pianist', year: 2002 },
-  { label: 'The Departed', year: 2006 },
-  { label: 'Terminator 2: Judgment Day', year: 1991 },
-  { label: 'Back to the Future', year: 1985 },
-  { label: 'Whiplash', year: 2014 },
-  { label: 'Gladiator', year: 2000 },
-  { label: 'Memento', year: 2000 },
-  { label: 'The Prestige', year: 2006 },
-  { label: 'The Lion King', year: 1994 },
-  { label: 'Apocalypse Now', year: 1979 },
-  { label: 'Alien', year: 1979 },
-  { label: 'Sunset Boulevard', year: 1950 },
-  {
-    label: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-    year: 1964
-  },
-  { label: 'The Great Dictator', year: 1940 },
-  { label: 'Cinema Paradiso', year: 1988 },
-  { label: 'The Lives of Others', year: 2006 },
-  { label: 'Grave of the Fireflies', year: 1988 },
-  { label: 'Paths of Glory', year: 1957 },
-  { label: 'Django Unchained', year: 2012 },
-  { label: 'The Shining', year: 1980 },
-  { label: 'WALL·E', year: 2008 },
-  { label: 'American Beauty', year: 1999 },
-  { label: 'The Dark Knight Rises', year: 2012 },
-  { label: 'Princess Mononoke', year: 1997 },
-  { label: 'Aliens', year: 1986 },
-  { label: 'Oldboy', year: 2003 },
-  { label: 'Once Upon a Time in America', year: 1984 },
-  { label: 'Witness for the Prosecution', year: 1957 },
-  { label: 'Das Boot', year: 1981 },
-  { label: 'Citizen Kane', year: 1941 },
-  { label: 'North by Northwest', year: 1959 },
-  { label: 'Vertigo', year: 1958 },
-  {
-    label: 'Star Wars: Episode VI - Return of the Jedi',
-    year: 1983
-  },
-  { label: 'Reservoir Dogs', year: 1992 },
-  { label: 'Braveheart', year: 1995 },
-  { label: 'M', year: 1931 },
-  { label: 'Requiem for a Dream', year: 2000 },
-  { label: 'Amélie', year: 2001 },
-  { label: 'A Clockwork Orange', year: 1971 },
-  { label: 'Like Stars on Earth', year: 2007 },
-  { label: 'Taxi Driver', year: 1976 },
-  { label: 'Lawrence of Arabia', year: 1962 },
-  { label: 'Double Indemnity', year: 1944 }
-];
