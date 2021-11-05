b57e296ce5dfc1f7c7c29e867258966be4cd0d94
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-11-06 00:29:36 +0530

    Removed Backdrop from eslint

--- src/theme/overrides/index.js
+++ src/theme/overrides/index.js
@@ -5,7 +5,6 @@ import Paper from './Paper';
 import Input from './Input';
 import Button from './Button';
 import Tooltip from './Tooltip';
-import Backdrop from './Backdrop';
 import Typography from './Typography';
 import IconButton from './IconButton';
 import Autocomplete from './Autocomplete';
@@ -20,7 +19,6 @@ export default function ComponentsOverrides(theme) {
     Input(theme),
     Button(theme),
     Tooltip(theme),
-    Backdrop(theme),
     Typography(theme),
     IconButton(theme),
     Autocomplete(theme)
