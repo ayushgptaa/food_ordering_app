0122b0dbd48040e25b6040ebcb6b5fbcfea9e2d5
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-11-06 21:00:06 +0530

    🔨 Available list fixed

---
+++ src/components/_dashboard/Menu/AvailableList.js
@@ -0,0 +1,69 @@
+/* eslint-disable camelcase */
+import { Grid, List, ListItem, ListItemText, Typography, IconButton } from '@mui/material';
+import ListSubheader from '@mui/material/ListSubheader';
+import PropTypes from 'prop-types';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { styled } from '@mui/material/styles';
+
+const ListContainer = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  boxShadow: theme.customShadows.z8,
+  borderRadius: theme.shape.borderRadius,
+  marginTop: '1rem'
+}));
+
+AvailableList.propTypes = {
+  categories: PropTypes.array,
+  handleOpenmodal: PropTypes.func,
+  deleteCategory: PropTypes.func
+};
+
+// ------------------------------------------------------
+export default function AvailableList({ categories, handleOpenmodal, deleteCategory }) {
+  return (
+    <Grid item xs={12} md={6}>
+      <ListContainer>
+        <List
+          dense={false}
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader" sx={{ borderRadius: 1 }}>
+              <Typography
+                sx={{
+                  mb: 2,
+                  pt: 2,
+                  color: 'grey[500]'
+                }}
+                variant="h6"
+                component="div"
+              >
+                Available Categories
+              </Typography>
+            </ListSubheader>
+          }
+        >
+          {categories &&
+            categories.map(({ category, category_id }, index) => {
+              return (
+                <ListItem
+                  key={category_id}
+                  secondaryAction={
+                    <>
+                      <IconButton edge="end" onClick={() => handleOpenmodal(category_id)}>
+                        <EditIcon aria-label="edit" />
+                      </IconButton>
+                      <IconButton edge="end" onClick={() => deleteCategory(category, category_id)}>
+                        <DeleteIcon aria-label="delete" />
+                      </IconButton>
+                    </>
+                  }
+                >
+                  <ListItemText primary={category} key={category_id} />
+                </ListItem>
+              );
+            })}
+        </List>
+      </ListContainer>
+    </Grid>
+  );
+}
