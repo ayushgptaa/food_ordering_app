1d5b82b4eae5a5588c05080f7d4b7d9cdcb0b156
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-11-07 21:58:29 +0530

    🧑‍🔬 Created TabsHeading

---
+++ src/components/_dashboard/Menu/Tabs/TabsHeading.js
@@ -0,0 +1,26 @@
+import { Grid, Typography } from '@mui/material';
+import PropTypes from 'prop-types';
+
+TabsHeading.propTypes = {
+  Heading: PropTypes.string
+};
+export default function TabsHeading({ Heading }) {
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ maxWidth: 'md', mx: 'auto' }}
+    >
+      <Typography
+        variant="h4"
+        component="span"
+        sx={{ opacity: 0.72, textAlign: 'center' }}
+        gutterBottom
+      >
+        {Heading}
+      </Typography>
+    </Grid>
+  );
+}
