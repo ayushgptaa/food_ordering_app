3263e9c5f3278ee73ea3c3a02fe67648f0df4b22
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-11-08 19:25:39 +0530

    completed add items

--- src/components/_dashboard/Menu/Tabs/AddItem.js
+++ src/components/_dashboard/Menu/Tabs/AddItem.js
@@ -1,21 +1,14 @@
 /* eslint-disable camelcase */
 import { useState } from 'react';
-import {
-  Select,
-  Grid,
-  Box,
-  InputAdornment,
-  MenuItem,
-  InputLabel,
-  FormControl
-} from '@mui/material';
+import { Select, Box, InputAdornment, MenuItem, InputLabel, FormControl } from '@mui/material';
 import PropTypes from 'prop-types';
 import CustomTextFeild from 'src/components/TextField';
 import LoadingButton from 'src/components/LoadingButton';
 import SnackBar from 'src/components/Snackbar';
 import Fetch from '../Fetch';
 import ItemList from '../ItemList';
-import TabsHeading from './TabsHeading';
+import Modal from '../ItemModal';
+import TabsContainer from '../TabsContainer';
 
 AddItem.propTypes = {
   categories: PropTypes.arrayOf(
@@ -37,6 +30,17 @@ export default function AddItem({ categories, getCategory }) {
   const [disabled, setDisabled] = useState(true);
   const [btnloading, setBtnloading] = useState(false);
   const [snackbar, setSnackbar] = useState({ severity: 'success', open: false, message: '' });
+  const [openmodal, setOpenmodal] = useState(false);
+  const [id, setid] = useState('');
+
+  const handleOpenmodal = (id) => {
+    setOpenmodal(true);
+    setid(id);
+  };
+
+  const handleClosemodal = () => {
+    setOpenmodal(false);
+  };
 
   const inputhandler = (e) => {
     const { value } = e.target;
@@ -66,12 +70,12 @@ export default function AddItem({ categories, getCategory }) {
       .then(() => {
         getCategory();
         setBtnloading(false);
+        setInput(defaultStates);
         setSnackbar({
           severity: 'success',
           open: true,
           message: `${input.item_name} added to the Categories :)`
         });
-        setInput(defaultStates);
       })
       .catch(() => {
         setBtnloading(false);
@@ -108,94 +112,110 @@ export default function AddItem({ categories, getCategory }) {
         });
       });
   };
+
+  // ************** EDIT ITEM FROM CATEGORY FUNCTION ***************** //
+
+  const editItem = async ({ item_name, item_description, item_price }, item_id) => {
+    const data = {
+      item_id: id,
+      item_name,
+      item_description,
+      item_price
+    };
+    Fetch(data, 'edit_item')
+      .then(() => {
+        getCategory();
+        handleClosemodal();
+        setSnackbar({
+          severity: 'success',
+          open: true,
+          message: `Changed Category to ${input} :)`
+        });
+      })
+      .catch(() => {
+        setSnackbar({
+          severity: 'error',
+          open: true,
+          message: `Unable to change category. Try again :(`
+        });
+      });
+  };
   return (
-    <>
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
+    <TabsContainer Heading="Add Item to Catrgory">
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Select Category</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={input.category_id}
+          name="category_id"
+          label="Select Category"
+          onChange={inputhandler}
+        >
+          {categories.map(({ category, category_id }) => {
+            return (
+              <MenuItem value={category_id} key={category_id}>
+                {category}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+
+      <CustomTextFeild
+        label="Item Title"
+        placeholder="Enter Item"
+        onChange={inputhandler}
+        name="item_name"
+      />
+      <CustomTextFeild
+        label="Item Description"
+        placeholder="Item Description"
+        multiline
+        rows={4}
+        onChange={inputhandler}
+        name="item_description"
+      />
+      <CustomTextFeild
+        margin="dense"
+        label="Amount"
+        placeholder="Enter Amount"
+        InputProps={{
+          endAdornment: <InputAdornment position="end">$</InputAdornment>
+        }}
+        onChange={inputhandler}
+        name="item_price"
+        type="number"
+      />
+      <LoadingButton
+        disabled={disabled}
+        addCategory={addItemtoCategory}
+        btnloading={btnloading}
+        loadingIndicator="Adding..."
+      >
+        ADD
+      </LoadingButton>
+
+      <Box
         sx={{
-          maxWidth: 350,
-          mx: 'auto'
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center'
         }}
       >
-        <TabsHeading Heading=" Add Item to Catrgory" />
-        <Box
-          sx={{
-            width: '100%',
-            mt: 1.5
-          }}
-        >
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Select Category</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={input.category_id}
-              name="category_id"
-              label="Category"
-              onChange={inputhandler}
-            >
-              {categories.map(({ category, category_id }) => {
-                return (
-                  <MenuItem value={category_id} key={category_id}>
-                    {category}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
-
-          <CustomTextFeild
-            label="Item Title"
-            placeholder="Enter Item"
-            onChange={inputhandler}
-            name="item_name"
-          />
-          <CustomTextFeild
-            label="Item Description"
-            placeholder="Item Description"
-            multiline
-            rows={4}
-            onChange={inputhandler}
-            name="item_description"
-          />
-          <CustomTextFeild
-            margin="dense"
-            label="Amount"
-            placeholder="Enter Amount"
-            InputProps={{
-              endAdornment: <InputAdornment position="end">$</InputAdornment>
-            }}
-            onChange={inputhandler}
-            name="item_price"
-            type="number"
-          />
-          <LoadingButton
-            disabled={disabled}
-            addCategory={addItemtoCategory}
-            btnloading={btnloading}
-            loadingIndicator="Adding..."
-          >
-            ADD
-          </LoadingButton>
-        </Box>
-        <Box
-          sx={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center'
-          }}
-        >
-          <ItemList categories={categories} deleteItemFromCategory={deleteItemFromCategory} />
-        </Box>
-      </Grid>
+        <ItemList
+          categories={categories}
+          deleteItemFromCategory={deleteItemFromCategory}
+          handleOpenmodal={handleOpenmodal}
+        />
+      </Box>
       <SnackBar
         open={snackbar.open}
         severity={snackbar.severity}
         handleClose={handleClose}
         message={snackbar.message}
       />
-    </>
+      <Modal open={openmodal} handleClose={handleClosemodal} editCategory={editItem} itemid={id} />
+    </TabsContainer>
   );
 }
