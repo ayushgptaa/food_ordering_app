3f9f14222848ba4c30e7a4362ef44e12b4d43ff4
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-11-13 19:40:24 +0530

    Channged Tabs width to 450

--- src/components/_dashboard/Menu/TabsContainer.js
+++ src/components/_dashboard/Menu/TabsContainer.js
@@ -2,15 +2,16 @@
 import { Grid, Box } from '@mui/material';
 import TabsHeading from './Tabs/TabsHeading';
 
-export default function TabsContainer({ Heading, children }) {
+export default function TabsContainer({ Heading, children, margintop }) {
   return (
     <Grid
       container
       direction="column"
       alignItems="center"
       sx={{
-        maxWidth: 400,
-        mx: 'auto'
+        maxWidth: 450,
+        mx: 'auto',
+        mt: 1
       }}
     >
       <TabsHeading Heading={Heading} />
