c1bba53bbe51b86db8c7d10a81bac6b3b5d508b4
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-11-25 14:11:18 +0530

    Created ItemsTable to display Items

---
+++ src/components/ItemsTable.js
@@ -0,0 +1,41 @@
+/* eslint-disable camelcase */
+import React from 'react';
+// material
+import { Table, TableRow, TableBody, TableCell, TableHead, Box, Collapse } from '@mui/material';
+
+import PropTypes from 'prop-types';
+
+ItemsTable.propTypes = {
+  items: PropTypes.array,
+  index: PropTypes.number,
+  selectedIndex: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
+
+export default function ItemsTable({ items, index, selectedIndex }) {
+  return (
+    <TableRow>
+      <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
+        <Collapse in={index === selectedIndex} timeout="auto" unmountOnExit>
+          <Box sx={{ margin: 1, mx: 'auto' }}>
+            <Table aria-label="items">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Item Name</TableCell>
+                  <TableCell align="center">Item Price</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {items.map(({ item_name, item_price }, index) => (
+                  <TableRow key={index}>
+                    <TableCell align="center">{item_name}</TableCell>
+                    <TableCell align="center">{item_price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Box>
+        </Collapse>
+      </TableCell>
+    </TableRow>
+  );
+}
