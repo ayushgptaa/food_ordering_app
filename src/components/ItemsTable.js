99f5c9ce2aad2a5ea0e12cbdecd593879af2147a
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-11-27 10:55:52 +0530

    added OptionsTable in ItemsTable

--- src/components/ItemsTable.js
+++ src/components/ItemsTable.js
@@ -1,7 +1,10 @@
 /* eslint-disable camelcase */
-import React from 'react';
+import { useState, Fragment } from 'react';
 // material
 import { Table, TableRow, TableBody, TableCell, TableHead, Box, Collapse } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 import PropTypes from 'prop-types';
 
@@ -14,6 +17,15 @@ ItemsTable.propTypes = {
 // -------------------------------------------------------------
 
 export default function ItemsTable({ items, index, selectedIndex }) {
+  const [selectedIndexoptions, setSelectedIndexoptions] = useState('');
+
+  const handleClick = (index) => {
+    if (selectedIndexoptions === index) {
+      setSelectedIndexoptions('');
+    } else {
+      setSelectedIndexoptions(index);
+    }
+  };
   return (
     <TableRow>
       <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
@@ -24,14 +36,37 @@ export default function ItemsTable({ items, index, selectedIndex }) {
                 <TableRow>
                   <TableCell align="center">Item Name</TableCell>
                   <TableCell align="center">Item Price</TableCell>
+                  <TableCell align="center">Options</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {items.map(({ item_name, item_price }, index) => (
-                  <TableRow key={index}>
-                    <TableCell align="center">{item_name}</TableCell>
-                    <TableCell align="center">{item_price}</TableCell>
-                  </TableRow>
+                {items.map(({ item_name, item_price, options }, index) => (
+                  <Fragment key={index}>
+                    <TableRow>
+                      <TableCell align="center">{item_name}</TableCell>
+                      <TableCell align="center">${item_price}</TableCell>
+                      <TableCell align="center">
+                        Options
+                        <IconButton
+                          aria-label="expand row"
+                          size="small"
+                          onClick={() => handleClick(index)}
+                        >
+                          {index === selectedIndexoptions ? (
+                            <KeyboardArrowUpIcon />
+                          ) : (
+                            <KeyboardArrowDownIcon />
+                          )}
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
+                    <OptionsTable
+                      index={index}
+                      options={options}
+                      selectedIndexoptions={selectedIndexoptions}
+                      key={index}
+                    />
+                  </Fragment>
                 ))}
               </TableBody>
             </Table>
@@ -41,3 +76,38 @@ export default function ItemsTable({ items, index, selectedIndex }) {
     </TableRow>
   );
 }
+
+const OptionsTable = ({ options, index, selectedIndexoptions }) => {
+  return (
+    <TableRow>
+      <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
+        <Collapse in={index === selectedIndexoptions} timeout="auto" unmountOnExit>
+          <Box sx={{ margin: 1, mx: 'auto' }}>
+            <Table aria-label="items">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Item Name</TableCell>
+                  <TableCell align="center">Item Price</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {options.map(({ option_name, option_price }, index) => (
+                  <TableRow key={index}>
+                    <TableCell align="center">{option_name}</TableCell>
+                    <TableCell align="center">{option_price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Box>
+        </Collapse>
+      </TableCell>
+    </TableRow>
+  );
+};
+
+OptionsTable.propTypes = {
+  options: PropTypes.array,
+  index: PropTypes.number,
+  selectedIndexoptions: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
