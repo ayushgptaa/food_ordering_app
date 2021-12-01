34c19d19c6e41c27fa0c63b49cedadd7a2f31cb4
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-12-02 00:17:10 +0530

    removed useless avatars

--- src/_mocks_/account.js
+++ src/_mocks_/account.js
@@ -3,7 +3,7 @@
 const account = {
   displayName: 'Ayush Gupta',
   email: 'Test123@gmail.com',
-  photoURL: '/static/mock-images/avatars/avatar_default.jpg'
+  photoURL: '/static/avatar_default.jpg'
 };
 
 export default account;
