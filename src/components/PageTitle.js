0f0d5bfe59b7cecf9f88e4e4f5475898a3a2cf25
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-12-01 23:32:28 +0530

    Added PageTitle in DashboardApp

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -16,7 +16,7 @@ export default function PageTitle({ title }) {
         textAlign: { xs: 'center' }
       }}
     >
-      <Typography variant="h3" gutterBottom sx={{ mb: 3, opacity: 0.7 }}>
+      <Typography variant="h3" gutterBottom sx={{ mb: 2, opacity: 0.7 }}>
         {title}
       </Typography>
     </Box>
--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -1,8 +1,9 @@
 // material
-import { Box, Grid, Container, Typography } from '@mui/material';
+import { Grid, Container } from '@mui/material';
+import PageTitle from 'src/components/PageTitle';
 // components
-import Page from '../components/Page';
 import { TodayAmount, TodayOrders, TotalOrders, TotalAmount } from '../components/_dashboard/app';
+import Page from '../components/Page';
 
 // ----------------------------------------------------------------------
 
@@ -10,15 +11,7 @@ export default function DashboardApp() {
   return (
     <Page title="Dashboard ">
       <Container maxWidth="xl">
-        <Box
-          sx={{
-            textAlign: { xs: 'center' }
-          }}
-        >
-          <Typography variant="h3" gutterBottom sx={{ opacity: 0.72 }}>
-            Hi, Welcome back
-          </Typography>
-        </Box>
+        <PageTitle title="Hi, Welcome back" />
         <Grid container spacing={3} sx={{ mt: { xs: 2, md: 0 } }}>
           <Grid item xs={12} sm={6}>
             <TodayAmount />
