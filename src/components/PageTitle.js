27897b48f50623f3c52d6982567e1c51b974df57
Ayush Gupta <ayush.gupta.jk@gmail.com>
2021-12-01 23:28:59 +0530

    🧪 created a seperate PageTitle component

---
+++ src/components/PageTitle.js
@@ -0,0 +1,24 @@
+// material
+import { Box, Typography } from '@mui/material';
+
+import PropTypes from 'prop-types';
+
+PageTitle.propTypes = {
+  title: PropTypes.string
+};
+
+// ------------------------------------------------
+
+export default function PageTitle({ title }) {
+  return (
+    <Box
+      sx={{
+        textAlign: { xs: 'center' }
+      }}
+    >
+      <Typography variant="h3" gutterBottom sx={{ mb: 3, opacity: 0.7 }}>
+        {title}
+      </Typography>
+    </Box>
+  );
+}
