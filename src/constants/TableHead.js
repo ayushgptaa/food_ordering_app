a76cbf6e1b5199a3b6d2f508ddaad32ac963bd5f
Ayush Gupta <ayush.gupta.jk@gmail.com>
2022-01-20 23:41:35 +0530

    added TABLE_HEAD in constant folder

---
+++ src/constants/TableHead.js
@@ -0,0 +1,13 @@
+const TABLE_HEAD = [
+  { id: 'Customername', label: 'Customer Name' },
+  { id: 'datecreated', label: 'Date Created' },
+  { id: 'carttotal', label: 'Cart Total' },
+  { id: 'totaldiscount', label: 'Total Discount' },
+  { id: 'discountedprice', label: 'Discounted Price' },
+  { id: 'taxamount', label: 'Tax Amount' },
+  { id: 'storeearned', label: 'Store Earned' },
+  { id: 'affiliatearned', label: 'Affiliate Earned' },
+  { id: 'items', label: 'Items' }
+];
+
+export default TABLE_HEAD;
