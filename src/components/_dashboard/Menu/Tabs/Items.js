adbb6e9ef6d638a6c2e0811bfecbaf02544f6301
Ayush Gupta <ayush.gupta.jk@gmail.com>
2022-02-05 21:13:33 +0530

    new validation for Items using yup

--- src/components/_dashboard/Menu/Tabs/Items.js
+++ src/components/_dashboard/Menu/Tabs/Items.js
@@ -1,6 +1,16 @@
 /* eslint-disable camelcase */
-import { useState, useContext } from 'react';
-import { Select, InputAdornment, MenuItem, InputLabel, FormControl } from '@mui/material';
+import { useContext } from 'react';
+import {
+  Select,
+  InputAdornment,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  FormHelperText
+} from '@mui/material';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 import CustomTextFeild from 'src/components/TextField';
 import LoadingButton from 'src/components/LoadingButton';
 import SnackBar from 'src/components/Snackbar';
@@ -24,55 +34,27 @@ export default function AddItem() {
     deletefn,
     editfn
   } = useContext(MenuContext);
-  const defaultStates = {
+
+  const initialValues = {
     item_name: '',
     item_description: '',
-    item_price: '0.00',
+    item_price: 0,
     category_id: ''
   };
-  const [input, setInput] = useState(defaultStates);
-  const [disabled, setDisabled] = useState(true);
-  const [error, setError] = useState(false);
-  const [helpertext, setHelpertext] = useState('');
 
-  const inputhandler = (e) => {
-    setError(false);
-    setHelpertext('');
-    let { value } = e.target;
-    value =
-      value.indexOf('.') >= 0
-        ? value.substr(0, value.indexOf('.')) + value.substr(value.indexOf('.'), 3)
-        : value;
-
-    setInput({
-      ...input,
-      [e.target.name]: value
-    });
-    setDisabled(false);
+  const getError = (feildName) => {
+    return touched[feildName] && Boolean(errors[feildName]);
   };
 
   // ************** ADD ITEM TO CATEGORY FUNCTION ***************** //
 
   const addItemtoCategory = async () => {
-    if (input.item_price === '' || input.item_name === '') {
-      setError(true);
-      setHelpertext('required');
-      return;
-    }
-    if (input.item_price > 500) {
-      setError(true);
-      setHelpertext("Can't add amount greater than $500");
-      return;
-    }
-    setInput(defaultStates);
-
     const data = {
-      ...input,
-      item_price: Number(input.item_price)
+      ...values
     };
 
-    const SuccessMsg = `${input.item_name} added to the Categories :)`;
-    const ErrorMsg = `Unable to add  ${input.item_name} to Categories. Try again :(`;
+    const SuccessMsg = `${values.item_name} added to the Categories :)`;
+    const ErrorMsg = `Unable to add  ${values.item_name} to Categories. Try again :(`;
     addfn('add_item', data, SuccessMsg, ErrorMsg);
   };
 
@@ -102,17 +84,30 @@ export default function AddItem() {
     editfn('edit_item', data, SuccessMsg, ErrorMsg);
   };
 
+  const { values, touched, errors, getFieldProps, handleSubmit } = useFormik({
+    initialValues,
+    validationSchema: Yup.object({
+      item_name: Yup.string().required('Item name is required'),
+      item_description: Yup.string(),
+      item_price: Yup.number()
+        .required('Item Price is required')
+        .min(0, "amount can't be less than $0")
+        .max(500, "Can't add amount greater than $500"),
+      category_id: Yup.string().required('Category Id is required')
+    }),
+    onSubmit: addItemtoCategory
+  });
+
   return (
     <TabsContainer Heading="Add Item to Category">
-      <FormControl fullWidth sx={{ mt: 1.5 }}>
-        <InputLabel id="demo-simple-select-label">Select Category</InputLabel>
+      <FormControl fullWidth sx={{ mt: 1.5 }} error={getError('category_id')}>
+        <InputLabel>Select Category</InputLabel>
         <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={input.category_id}
+          id="category_id"
           name="category_id"
           label="Select Category"
-          onChange={inputhandler}
+          required
+          {...getFieldProps('category_id')}
         >
           {categories.map(({ category, category_id }) => {
             return (
@@ -122,48 +117,46 @@ export default function AddItem() {
             );
           })}
         </Select>
+        <FormHelperText error={getError('category_id')}>
+          {(getError('category_id') && errors.category_id) || ''}
+        </FormHelperText>
       </FormControl>
 
       <CustomTextFeild
         label="Item Title"
         placeholder="Enter Item"
-        onChange={inputhandler}
+        id="item_name"
         name="item_name"
-        value={input.item_name}
+        {...getFieldProps('item_name')}
         required
-        error={input.item_name ? false : error}
-        helperText={input.item_name ? '' : helpertext}
+        error={getError('item_name')}
+        helperText={errors.item_name || ''}
       />
       <CustomTextFeild
         margin="dense"
         label="Amount"
         placeholder="Enter Amount"
+        min={0}
         InputProps={{
           endAdornment: <InputAdornment position="end">$</InputAdornment>
         }}
-        onChange={inputhandler}
+        id="item_price"
         name="item_price"
         type="number"
-        value={input.item_price}
-        required
-        error={input.item_price && input.item_price < 500 ? false : error}
-        helperText={input.item_price && input.item_price < 500 ? '' : helpertext}
+        {...getFieldProps('item_price')}
+        error={getError('item_price')}
+        helperText={errors.item_price || ''}
       />
       <CustomTextFeild
         label="Item Description"
         placeholder="Item Description"
         multiline
         rows={3}
-        onChange={inputhandler}
+        id="item_description"
         name="item_description"
-        value={input.item_description}
+        {...getFieldProps('item_description')}
       />
-      <LoadingButton
-        disabled={disabled}
-        addCategory={addItemtoCategory}
-        btnloading={btnloading}
-        loadingIndicator="Adding..."
-      >
+      <LoadingButton onClick={handleSubmit} btnloading={btnloading} loadingIndicator="Adding...">
         ADD
       </LoadingButton>
 
